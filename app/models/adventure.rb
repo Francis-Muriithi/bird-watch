7fb8390a588d88219b6550d34726a7bd025172d9
Francis-Muriithi <mufraggie@gmail.com>
2022-12-10 11:55:12 +0300

    initial commit, database, seeds

---
+++ app/models/adventure.rb
@@ -0,0 +1,15 @@
+class Adventure < ApplicationRecord
+    has_many :bird:scale => 
+
+
+    before_create :slugify  
+    
+# the slugify method helps that when we create the airline name, we make it a slug that converts the title to the slug value
+    def slugify 
+        self.slug = title.parameterize
+    end
+
+    def avg_likes 
+        reviews.average(:score).round(2).to_f
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+adventures = Adventure.create([
+    {
+        title: "Kilimambogo Hike",
+        image_url: "https://alchetron.com/cdn/ol-donyo-sabuk-b09cbac9-1d48-4f0d-b8d0-248709fda39-resize-750.jpeg",
+    },
+    {
+        title: "Ngong Hills Trip",
+        image_url: "http://howandwhere.co.ke/outdoorskenya/wp-content/uploads/sites/3/2017/05/ngonghillskenya-jpg.jpeg",
+    }, 
+    {
+        title: "Mt Longonot Day Trip",
+        image_url: "https://bountifulsafaris.com/wp-content/uploads/2016/05/Mt-Longonot_the-crater.jpg",
+    }
+
+
+])
