399eb2cc02f7bde6a0ec4649ca560d8f33345092
Francis-Muriithi <mufraggie@gmail.com>
2022-12-10 13:10:46 +0300

    update methods in controllers

--- app/controllers/api/v1/adventures_controller.rb
+++ app/controllers/api/v1/adventures_controller.rb
@@ -4,19 +4,19 @@ module Api
 
             def index
                 adventures = Adventure.all
-                render json: AdventureSerializer.new(adventures, options).serialize_json
+                render json: AdventureSerializer.new(adventures, options).serialized_json
             end
 
             def show 
                 adventure = Adventure.find_by(slug: params[:slug])
-                render json: AdventureSerializer.new(adventure, options).serialize_json
+                render json: AdventureSerializer.new(adventure, options).serialized_json
             end
 
             def create 
                 adventure = Adventure.new(adventure_params)
 
                 if adventure.save 
-                    render json: AdventureSerializer.new(adventure).serialize_json
+                    render json: AdventureSerializer.new(adventure).serialized_json
                 else
                     render json: {error: adventure.errors.messages}, status: 422
                 end
@@ -26,7 +26,7 @@ module Api
                 adventure = Adventure.find_by(slug: params[:slug])
 
                 if adventure.update(adventure_params)
-                    render json: AdventureSerializer.new(adventure, options).serialize_json
+                    render json: AdventureSerializer.new(adventure, options).serialized_json
                 else
                     render json: {error: adventure.errors.messages}, status: 422
                 end
--- app/controllers/api/v1/birds_controller.rb
+++ app/controllers/api/v1/birds_controller.rb
@@ -2,11 +2,30 @@ module Api
     module v1
         class BirdsController < ApplicationController
 
+            def create 
+                bird = Bird.new(bird_params)
+
+                if bird.save 
+                    render json: BirdSerializer.new(bird).serialized_json
+                else
+                    render json: {error: bird.errors.messages}, status: 422
+                end
+            end
 
 
-            
+            def destroy
+                bird= Bird.find_by(params[:id])
+
+                if bird.destroy
+                   head :no_content
+                else
+                    render json: {error: bird.errors.messages}, status: 422
+                end
+            end
+
+
             private
-            def birds_params 
+            def bird_params 
                 params.require(:bird).permit(:name, :image_url, :description, :likes, :adventure_id)
             end
             
