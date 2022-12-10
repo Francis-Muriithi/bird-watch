7cdd1ba78a08749fe9d3eaf4d5d61059f4d1d7aa
Francis-Muriithi <mufraggie@gmail.com>
2022-12-10 13:00:50 +0300

    add methods to adventures_controller

--- app/controllers/api/v1/adventures_controller.rb
+++ app/controllers/api/v1/adventures_controller.rb
@@ -1,7 +1,57 @@
 module Api
-    module v1
+    module V1
         class AdventureController < ApplicationController
-            
+
+            def index
+                adventures = Adventure.all
+                render json: AdventureSerializer.new(adventures, options).serialize_json
+            end
+
+            def show 
+                adventure = Adventure.find_by(slug: params[:slug])
+                render json: AdventureSerializer.new(adventure, options).serialize_json
+            end
+
+            def create 
+                adventure = Adventure.new(adventure_params)
+
+                if adventure.save 
+                    render json: AdventureSerializer.new(adventure).serialize_json
+                else
+                    render json: {error: adventure.errors.messages}, status: 422
+                end
+            end
+
+            def update 
+                adventure = Adventure.find_by(slug: params[:slug])
+
+                if adventure.update(adventure_params)
+                    render json: AdventureSerializer.new(adventure, options).serialize_json
+                else
+                    render json: {error: adventure.errors.messages}, status: 422
+                end
+            end
+
+            def destroy
+                adventure = Adventure.find_by(slug: params[:slug])
+
+                if adventure.destroy
+                   head :no_content
+                else
+                    render json: {error: adventure.errors.messages}, status: 422
+                end
+            end
+
+# Private Methods
+        private 
+        def adventure_params 
+            params.require(:adventure).permit(:title, :image_url)
+        end
+
+        def options 
+            @options ||= {include: %i[birds]}
+        end
+
         end
     end
 end
