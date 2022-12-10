fbd9d944ff398dfb0cf1522d4ad35c569deaed0c
Francis-Muriithi <mufraggie@gmail.com>
2022-12-10 13:42:01 +0300

    test routes and endpoints

--- app/controllers/api/v1/adventures_controller.rb
+++ app/controllers/api/v1/adventures_controller.rb
@@ -1,6 +1,6 @@
 module Api
     module V1
-        class AdventureController < ApplicationController
+        class AdventuresController < ApplicationController
 
             def index
                 adventures = Adventure.all
