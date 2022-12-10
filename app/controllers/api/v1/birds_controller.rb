983acf64f3f8db274b84ce528a6f7e1abc7ddee1
Francis-Muriithi <mufraggie@gmail.com>
2022-12-10 13:03:21 +0300

    add private method to birds_controller

--- app/controllers/api/v1/birds_controller.rb
+++ app/controllers/api/v1/birds_controller.rb
@@ -1,6 +1,14 @@
 module Api
     module v1
         class BirdsController < ApplicationController
+
+
+
+            
+            private
+            def birds_params 
+                params.require(:bird).permit(:name, :image_url, :description, :likes, :adventure_id)
+            end
             
         end
     end
